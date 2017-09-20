fe76c8ad3ba8ce738550bbb0b07cf8ee5a08de93
Jacob Wylie <jacobwylie@gmail.com>
2017-09-20 15:52:02 +0200

    Add files via upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'ddd34ce11757e6e79f50da0645aa5675';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
