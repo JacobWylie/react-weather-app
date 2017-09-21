8a07e993c4976801f7a1d7cba59700d7e136a941
Jacob Wylie <jacobwylie@gmail.com>
2017-09-21 16:56:53 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,51 @@
+// module.exports = {
+//   entry: [
+//     './src/index.js'
+//   ],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [{
+//       exclude: /node_modules/,
+//       loader: 'babel',
+//       query: {
+//         presets: ['react', 'es2015', 'stage-1']
+//       }
+//     }]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './'
+//   }
+// };
+
+
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: './src/index.js',
+  
   output: {
-    path: __dirname,
-    publicPath: '/',
+    // path: 'build',
     filename: 'bundle.js'
   },
+  
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader'] 
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader' 
       }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    ]
   }
-};
+}
